8b791c11d84a7e1e002cda5077f46ffa09226322
rirakib10 <rirrakib10@gmail.com>
2023-01-07 22:28:17 +0600

    problem solved

---
+++ smallerNumbersThanCurrent.js
@@ -0,0 +1,16 @@
+
+var smallerNumbersThanCurrent = function(nums) {
+    let arr = []
+
+    for (let i = 0; i < nums.length; i++) {
+        let count = 0;
+        for (let j = 0; j < nums.length; j++) {
+            if(nums[i]> nums[j]) count++
+        }
+        arr.push(count)
+    }
+
+    return arr;
+};
+
+console.log(smallerNumbersThanCurrent([7,7,7,7]))
