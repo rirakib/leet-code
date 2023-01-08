f8d2a1f2847578bc5bd01646f3a3a730e841524e
rirakib10 <rirrakib10@gmail.com>
2023-01-08 21:05:25 +0600

    problem solved

---
+++ restoreString.js
@@ -0,0 +1,12 @@
+var restoreString = function(s, indices) {
+    
+    let str = '';
+
+    for (let i = 0; i < indices.length; i++) {
+      str += s[indices.indexOf(i)]
+    }
+
+    return str;
+};
+restoreString("codeleet",[4,5,6,7,0,2,1,3])
+
